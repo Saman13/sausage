4007b7214cb73b7885dea7aefc467fac0eaeedf6
std-030-13 std-030-13 <std-030-13@praktikum-services.ru>
2024-08-06 09:56:06 +0000

    Update deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -5,7 +5,7 @@ set -xe
 sudo cp -rf backend.service /etc/systemd/system/backend.service
 sudo rm -f /home/student/sausage-store.jar||true
 #Переносим артефакт в нужную папку
-curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/repository/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
+sudo curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/repository/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store.jar /opt/sausage-store/bin/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
 #Обновляем конфиг systemd с помощью рестарта
 sudo systemctl daemon-reload
