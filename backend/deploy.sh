6be152cd842e650e1af9fff9cc2a0618895026a0
Saman <samanpunk@yandex.ru>
2024-08-07 10:18:22 +0300

    add variables

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -6,9 +6,9 @@ sudo cp -rf backend.service /etc/systemd/system/backend.service
 sudo rm -f /home/student/sausage-store.jar||true
 sudo rm -f /opt/sausage-store/bin/sausage-store.jar||true
 #создадим файл с переменными
-sudo rm -f /opt/sausage-store/bin/backend.env||true
-sudo echo DB_PASS=${DB_PASS} > /opt/sausage-store/bin/backend.env
-sudo echo DB_USER=${DB_USER} >> /opt/sausage-store/bin/backend.env
+sudo rm -f /home/student/backend.env||true
+sudo echo DB_PASS=${DB_PASS} > /home/student/backend.env
+sudo echo DB_USER=${DB_USER} >> /home/student//backend.env
 #Переносим артефакт в нужную папку
 sudo curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/repository/${NEXUS_REPO_BACKEND_NAME}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp ./sausage-store.jar /opt/sausage-store/bin/sausage-store.jar||true #"<...>||true" говорит, если команда обвалится — продолжай
