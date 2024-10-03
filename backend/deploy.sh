e1c0bda3d305718cf5bd71533c4586c649119549
Saman <samanpunk@yandex.ru>
2024-10-03 13:01:45 +0300

    test pipeline

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -28,14 +28,14 @@ blue_running=$(is_container_running $BLUE_CONTAINER)
 if [ "$green_running" = "true" ] && [ "$green_status" = "healthy" ]; then
     echo "Green контейнер запущен и работает. Перезапускаем на Blue..."
     docker --context remote compose --env-file deploy.env up backend_blue -d --pull "always" --force-recreate
-    docker stop $GREEN_CONTAINER
-    docker rm $GREEN_CONTAINER
+    docker --context remote stop $GREEN_CONTAINER
+    docker --context remote rm $GREEN_CONTAINER
 
 elif [ "$blue_running" = "true" ] && [ "$blue_status" = "healthy" ]; then
     echo "Blue контейнер запущен и работает. Перезапускаем на Green..."
     docker --context remote compose --env-file deploy.env up backend_green -d --pull "always" --force-recreate
-    docker stop $BLUE_CONTAINER
-    docker rm $BLUE_CONTAINER
+    docker --context remote stop $BLUE_CONTAINER
+    docker --context remote rm $BLUE_CONTAINER
 
 elif [ "$green_running" = "false" ] && [ "$blue_running" = "false" ]; then
     echo "Ни один контейнер не запущен. Запускаем Green..."
@@ -43,8 +43,8 @@ elif [ "$green_running" = "false" ] && [ "$blue_running" = "false" ]; then
 
 elif [ "$green_running" = "true" ] && [ "$blue_running" = "true" ]; then
     echo "Оба контейнера запущены. Останавливаем Blue..."
-    docker stop $BLUE_CONTAINER
-    docker rm $BLUE_CONTAINER
+    docker --context remote stop $BLUE_CONTAINER
+    docker --context remote rm $BLUE_CONTAINER
 else
     echo "Состояние контейнеров не определено. Запускаем green."
     docker --context remote compose --env-file deploy.env up backend_green -d --pull "always" --force-recreate
